65052520034c54cf77a8531fe99eff62b34d25b6
matteo <matteolucantonio@gmail.com>
2020-11-18 18:45:49 +0100

    Asymmetric state variables and actions. DQN can handle more than 32 input. Runtime random post-conditions

--- src/main/java/com/secureai/DynDQNMain.java
+++ src/main/java/com/secureai/DynDQNMain.java
@@ -140,7 +140,7 @@ public class DynDQNMain {
                 Integer.parseInt(argsMap.getOrDefault("maxStep", "250000")),           //Max step
                 Integer.parseInt(argsMap.getOrDefault("expRepMaxSize", "5000")),      //Max size of experience replay
                 Integer.parseInt(argsMap.getOrDefault("batchSize", "128")),           //size of batches
-                Integer.parseInt(argsMap.getOrDefault("targetDqnUpdateFreq", "1000")), //target update (hard)
+                Integer.parseInt(argsMap.getOrDefault("targetDqnUpdateFreq", "10000")), //target update (hard)
                 Integer.parseInt(argsMap.getOrDefault("updateStart", "100")),           //num step noop warmup
                 Double.parseDouble(argsMap.getOrDefault("rewardFactor", "1")),        //reward scaling
                 Double.parseDouble(argsMap.getOrDefault("gamma", "0.75")),            //gamma
